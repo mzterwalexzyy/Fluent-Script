efb66794a1fcfa175b72a69f0b667599de7a0c96
Zenith <161211651+dxzenith@users.noreply.github.com>
2024-08-15 15:25:31 +0530

    Update fluent-devnet-contract.sh

--- fluent-devnet-contract.sh
+++ fluent-devnet-contract.sh
@@ -7,24 +7,34 @@ YELLOW=$(tput setaf 3)
 print_command() {
   echo -e "${BOLD}${YELLOW}$1${RESET}"
 }
-echo
-echo
 
-cat << "EOF"
-███████ ██    ██ ███    ██ ██   ██ ██████  ████████ 
-   ███  ██    ██ ████   ██  ██ ██  ██   ██    ██    
-  ███   ██    ██ ██ ██  ██   ███   ██████     ██    
- ███    ██    ██ ██  ██ ██  ██ ██  ██   ██    ██    
-███████  ██████  ██   ████ ██   ██ ██████     ██    
-EOF
-
-echo
-echo
-
-sleep 2
-
-# Change to home directory
 cd $HOME
+
+print_command "Installing Cargo..."
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+. "$HOME/.cargo/env"
+
+print_command "Installing NVM and Node..."
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+
+if [ -s "$NVM_DIR/nvm.sh" ]; then
+    . "$NVM_DIR/nvm.sh"
+elif [ -s "/usr/local/share/nvm/nvm.sh" ]; then
+    . "/usr/local/share/nvm/nvm.sh"
+else
+    echo "Error: nvm.sh not found!"
+    exit 1
+fi
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+print_command "Using Node version manager (nvm)..."
+nvm install node
+nvm use node
+
+node -v
+npm -v
 
 print_command "Installing gblend tool..."
 cargo install gblend
